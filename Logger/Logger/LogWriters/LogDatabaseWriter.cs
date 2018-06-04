61564060e4b660cd27d521a5a1b8f77d773f1219
Nicholas Ghobrial <nicholasghob@yahoo.com>
2018-06-04 00:39:53 -0400

    Fix failed build due to incorrect constructors

--- Logger/Logger/LogWriters/LogDatabaseWriter.cs
+++ Logger/Logger/LogWriters/LogDatabaseWriter.cs
@@ -8,25 +8,15 @@ namespace LyeltLogger
 {
     public class LogDatabaseWriter : LogWriter
     {
-        private LogDatabaseOptions _options;
+        public LogDatabaseWriter(string name) : this(name, LogOptions.Default) { }
 
-        public LogDatabaseWriter(string name) : this(name, LogOptions.Default, LogDatabaseOptions.Default) { }
-
-        public LogDatabaseWriter(string name, LogOptions common) : this(name, common, LogDatabaseOptions.Default) { }
-
-        public LogDatabaseWriter(string name, LogOptions common, LogDatabaseOptions options) : base(name, common)
+        public LogDatabaseWriter(string name, LogOptions common) : base(name, common)
         {
-            _options = options;
         }
 
         public override void LogMessage(LogMessage message)
         {
 
-        }
-
-        public override void SetWriterOptions(WriterOptions options)
-        {
-            
         }
     }
 }
--- Logger/Logger/LogWriters/LogEventViewerWriter.cs
+++ Logger/Logger/LogWriters/LogEventViewerWriter.cs
@@ -8,23 +8,15 @@ namespace LyeltLogger
 {
     public class LogEventViewerWriter : LogWriter
     {
-        public LogEventViewerWriter(string name) : this(name, LogOptions.Default, LogEventViewerOptions.Default) { }
-
-        public LogEventViewerWriter(string name, LogOptions common) : this(name, common, LogEventViewerOptions.Default) { }
-
-        public LogEventViewerWriter(string name, LogOptions common, LogEventViewerOptions options) : base(name, common)
+        public LogEventViewerWriter(string name) : this(name, LogOptions.Default) { }
+        
+        public LogEventViewerWriter(string name, LogOptions common) : base(name, common)
         {
-
         }
 
         public override void LogMessage(LogMessage message)
         {
 
-        }
-
-        public override void SetWriterOptions(WriterOptions options)
-        {
-            
         }
     }
 }
